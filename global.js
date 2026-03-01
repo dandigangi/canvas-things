91fc74d01c2dbb7dbb74a3746a7ab06262991af6
dandigangi <me@dandigangi.com>
2026-03-01 11:31:32 -0600

    cleanup, prettier, separate css/js globals for cleanliness so i dont go crazy

---
+++ global.js
@@ -0,0 +1,23 @@
+const CONSTANTS = {
+   canvasWidth: 800,
+   canvasHeight: 600,
+}
+
+// Set watermark to experiment title
+;(function () {
+   const el = document.getElementById('title')
+   el.textContent = el.textContent.replace(
+      'Loading',
+      document.title.replace(/ canvas things/i, ''),
+   )
+})()
+
+// Supporting functions
+function rand(max = null) {
+   if (!max) throw new Error('[fx-rand] No max value provided')
+   return Math.floor(Math.random() * max + 3)
+}
+
+function randRgb() {
+   return `rgb(${rand(255)}, ${rand(255)}, ${rand(255)})`
+}
